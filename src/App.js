873dba4c92f4b5c8bcb0b6c3b5416fcdbc0d4078
ovgun tas <ovgun.tas@argedor.com>
2024-12-04 23:28:39 +0300

    rick and morty componenti

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import RickAndMorty from "./components/rickAndMorty";
 import Users from "./components/users";
 
 function App() {
   return (
     <div className="App">
       <Users />
+      <RickAndMorty />
     </div>
   );
 }
---
+++ src/components/RickAndMorty.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RickAndMorty = () => {
+  console.log("Bu yeni sayfaya giriş yaptınız");
+  return (
+    <div>
+      <h1>Burada Rick and Morty Karakterlerini listeleyeceğiz.</h1>
+    </div>
+  );
+};
+
+export default RickAndMorty;
